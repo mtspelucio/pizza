63c2982f08c89e8acbaa129a32e47292bbfc01f8
Matheus Pelucio <mts.rangel@hotmail.com>
2023-09-20 22:47:27 -0300

    update Repository

--- src/main/java/com/pizza/cardapio/repositories/OrderRepository.java
+++ src/main/java/com/pizza/cardapio/repositories/OrderRepository.java
@@ -2,9 +2,10 @@ package com.pizza.cardapio.repositories;
 
 import com.pizza.cardapio.models.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepositori extends MongoRepository<Order, String> {
+public interface OrderRepository extends MongoRepository<Order, String> {
 
 }
