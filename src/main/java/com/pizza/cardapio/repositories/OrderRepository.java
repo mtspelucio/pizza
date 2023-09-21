4be068dea6a525ed9a3cb570935a617e7ef747ce
Matheus Pelucio <mts.rangel@hotmail.com>
2023-09-20 22:53:33 -0300

    update to return data to mogoDB

--- src/main/java/com/pizza/cardapio/repositories/OrderRepository.java
+++ src/main/java/com/pizza/cardapio/repositories/OrderRepository.java
@@ -2,7 +2,6 @@ package com.pizza.cardapio.repositories;
 
 import com.pizza.cardapio.models.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
